e327d41d53f8a2e165a7b34e8328e5cb32042afb
Loïc Weinhard <loic.weinhard@neofacto.com>
2020-03-08 19:29:33 +0100

    Feature : add PriceCalculator class and its tests

---
+++ backend/level1/spec/price_calculator_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe PriceCalculator do
+  let(:hash) { parse_from_json_input }
+  let(:rentals_hash) { hash["rentals"] }
+  let(:cars_hash) { hash["cars"] }
+  let(:rentals) { RentalFactory.generate(rentals_hash) }
+  let(:cars) { CarFactory.generate(cars_hash) }
+  let(:price_calculator) { PriceCalculator.new(cars, rentals) }
+  let(:prices) { price_calculator.compute_prices }
+
+  it "should compute correct rental price id" do
+    expect(prices.first[:id]).to eq rentals.first.id
+  end
+
+  it "should compute correct price" do
+    expect(prices.first[:price]).to eq 7000
+  end
+
+  it "should contain correct amount of rentals prices" do
+    expect(prices.length).to eq rentals.length
+  end
+end
---
+++ backend/level1/src/price_calculator.rb
@@ -0,0 +1,28 @@
+class PriceCalculator
+  attr_reader :cars, :rentals
+  attr_accessor :prices
+
+  def initialize(cars, rentals)
+    @cars = cars
+    @rentals = rentals
+  end
+
+  def compute_prices
+    @prices = @rentals.map do |rental|
+      car = fetch_car_from_id(rental.id)
+      compute_price(rental, car)  
+    end
+  end
+
+  def fetch_car_from_id(id)
+    @cars.find { |car| car.id === id }
+  end
+
+  def compute_price(rental, car)
+    duration_price = rental.duration * car.price_per_day
+    kms_price = rental.distance * car.price_per_km
+    total_price = duration_price + kms_price
+
+    { id: rental.id, price: total_price }
+  end
+end
