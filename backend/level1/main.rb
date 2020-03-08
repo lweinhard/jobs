43ecf71194505ffd46d00586798e538df6199aef
Loïc Weinhard <loic.weinhard@neofacto.com>
2020-03-08 19:38:06 +0100

    Feature : add logic to main

--- backend/level1/main.rb
+++ backend/level1/main.rb
@@ -1,7 +1,13 @@
-require './json_input_reader.rb'
+require './loader.rb'
 
 def main
+  output = {}
   hash = parse_from_json_input
+  cars = CarFactory.generate(hash["cars"])
+  rentals = RentalFactory.generate(hash["rentals"])
+  price_calculator = PriceCalculator.new(cars, rentals)
+  output["rentals"] = price_calculator.compute_prices
+  write_output_to_file(output)
 end
 
 main
